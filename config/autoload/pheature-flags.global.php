61af8ef53ecbc23cc8b39003e273db7896fcec5b
Xavier Serrat Bordas <xavier.serrat22@gmail.com>
2022-01-15 21:45:36 +0100

    Fix feature name in migration and enable "chain" driver to load inmemmory and dbal toggles

--- config/autoload/pheature-flags.global.php
+++ config/autoload/pheature-flags.global.php
@@ -4,6 +4,17 @@ declare(strict_types=1);
 
 return [
     'pheature_flags' => [
-        'driver' => 'dbal',
+        'driver' => 'chain',
+        'driver_options' => ['inmemory', 'dbal'],
+        'toggles' => [
+            [
+                'id' => 'some_feature_section',
+                'enabled' => true,
+            ],
+            [
+                'id' => 'in_progress_feature_section',
+                'enabled' => true,
+            ]
+        ]
     ],
 ];
